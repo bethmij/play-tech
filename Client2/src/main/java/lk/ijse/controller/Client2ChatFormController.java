241cac9a34e470e0da8f710f835eaed762def3de
Dinarbrooke <bethmij@gmail.com>
2023-06-21 01:07:43 +0530

    modified client2 chat room

--- Client2/src/main/java/lk/ijse/controller/Client2ChatFormController.java
+++ Client2/src/main/java/lk/ijse/controller/Client2ChatFormController.java
@@ -3,6 +3,7 @@ package lk.ijse.controller;
 import com.jfoenix.controls.JFXButton;
 import javafx.event.ActionEvent;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 
 public class Client2ChatFormController {
 
@@ -19,5 +20,8 @@ public class Client2ChatFormController {
     }
 
     public void btnEmojiOnAction(ActionEvent actionEvent) {
+    }
+
+    public void VBoxOnAction(MouseEvent mouseEvent) {
     }
 }
